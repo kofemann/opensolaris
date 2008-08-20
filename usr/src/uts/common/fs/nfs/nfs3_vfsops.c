0fd7ff5c87dbcbf2114bc409e58f1a105388b6a1
Thomas Haynes <Thomas.Haynes@Sun.COM>
2008-08-20 10:52:04 -0500

    Another quick test of gatekeeping code.

--- usr/src/uts/common/fs/nfs/nfs3_vfsops.c
+++ usr/src/uts/common/fs/nfs/nfs3_vfsops.c
@@ -19,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -27,8 +27,6 @@
  *	Copyright (c) 1983,1984,1985,1986,1987,1988,1989  AT&T.
  *	All rights reserved.
  */
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 #include <sys/param.h>
 #include <sys/types.h>
